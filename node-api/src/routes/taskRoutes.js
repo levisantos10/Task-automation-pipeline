b6b07e9cb49af8a10756c5e0256a9e3bc6f4eb24
levisantos10 <santtoslevi11@gmail.com>
2026-02-19 23:15:14 -0400

    feat: adiciona uma nova rota

--- node-api/src/routes/taskRoutes.js
+++ node-api/src/routes/taskRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTasks, getTaskById, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { 
+  getAllTasks, 
+  getTaskById, 
+  createTask, 
+  updateTask, 
+  deleteTask, 
+  toggleTaskStatus 
+} = require('../controllers/taskController');
 
 router.get('/', getAllTasks);
 router.get('/:id', getTaskById);
 router.post('/', createTask);
 router.put('/:id', updateTask);
+router.patch('/:id/toggle', toggleTaskStatus);  // adiciona nova rota
 router.delete('/:id', deleteTask);
 
 module.exports = router;
